de78b0ca3346dbaf1c9caaf8a02ee1cc3e260548
Kiterai <kiterai@foolslab.net>
2022-04-05 23:42:26 +0900

    Charactorクラスにおけるウィンドウ関連の処理を関数化

--- src/Charactor.cpp
+++ src/Charactor.cpp
@@ -3,9 +3,18 @@
 #include <gl/GL.h>
 
 Charactor::Charactor(const Window *_mainWindow)
-    : mainWindow(_mainWindow), tex1("./assets/test.png") {}
+    : mainWindow(_mainWindow), tex1("./assets/test.png") {
+    setWindowSize({512, 512});
+}
 
 Charactor::~Charactor() {}
+
+void Charactor::setWindowPos(Point pos) { mainWindow->setWindowPos(pos); }
+
+void Charactor::setWindowSize(Size size) {
+    mainWindow->setWindowSize(size);
+    glViewport(0, 0, size.x, size.y);
+}
 
 void Charactor::update() {}
 
