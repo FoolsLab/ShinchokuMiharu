54221710e75530ccaef60a14ad2dee2c71e5db85
kegra <nukegarara@yahoo.co.jp>
2022-04-05 20:19:50 +0900

    テクスチャ描画機能(WIP)+描画テスト

--- src/utils/Texture.hpp
+++ src/utils/Texture.hpp
@@ -1,12 +1,18 @@
 #pragma once
 
+#include "./utils.hpp"
 #include <GLFW/glfw3.h>
 #include <string>
 
 class Texture {
   private:
     GLuint texName;
+    Size size;
+
   public:
     Texture(std::string path, GLint filter = GL_LINEAR);
     ~Texture();
+
+    void Draw(const Point dst);
+    void Draw(const Point dst, const Point src, const Size srcRect);
 };
