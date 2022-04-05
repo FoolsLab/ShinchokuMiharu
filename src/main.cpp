6cd943607fbb9ba3d596aefeb2fc0c94994d01c4
Kiterai <kiterai@foolslab.net>
2022-04-05 10:19:54 +0900

    モニタ情報の取得

--- src/main.cpp
+++ src/main.cpp
@@ -41,12 +41,35 @@ struct Monitor {
         return origin.x <= pos.x && pos.x < origin.x + size.x &&
                origin.y <= pos.y && pos.y < origin.y + size.y;
     }
+
+    Monitor(GLFWmonitor *glfwMonitor) {
+        const GLFWvidmode *videoMode = glfwGetVideoMode(glfwMonitor);
+        size.x = videoMode->width;
+        size.y = videoMode->height;
+
+        Vec2<int> tmpPos;
+        glfwGetMonitorPos(glfwMonitor, &tmpPos.x, &tmpPos.y);
+        origin = tmpPos;
+
+        Vec2<float> tmpScale;
+        glfwGetMonitorContentScale(glfwMonitor, &tmpScale.x, &tmpScale.y);
+        scale = tmpScale;
+    }
 };
 
 class MonitorManager {
     std::vector<Monitor> monitors;
 
   public:
+    void update() {
+        int count;
+        auto pMonitors = glfwGetMonitors(&count);
+        monitors.resize(count);
+        for (size_t i = 0; i < count; i++) {
+            monitors[i] = Monitor(pMonitors[i]);
+        }
+    }
+
     int getMonitorIndexFromCoord(const Point pos) const {
         for (int i = 0; const auto &monitor : monitors) {
             if (monitor.isCoordInMonitor(pos)) {
@@ -95,6 +118,8 @@ class Window {
 class System {
     Window mainWindow;
     Point cursorPos;
+
+    MonitorManager monitorManager;
 
     void updateCursourPos() {
         cursorPos = mainWindow.getCursorPos() + mainWindow.getWindowPos();
