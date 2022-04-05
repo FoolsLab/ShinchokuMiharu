a068cf982668017c01f0adcf7577f1aa2a10b27c
kegra <nukegarara@yahoo.co.jp>
2022-04-05 22:48:09 +0900

    ウィンドウ情報をCharactorに渡す

--- src/Charactor.hpp
+++ src/Charactor.hpp
@@ -1,13 +1,16 @@
 #pragma once
 
+#include "common/Window.hpp"
 #include "utils/Texture.hpp"
+#include <memory>
 
 class Charactor {
   private:
+    const Window *mainWindow;
     Texture tex1;
 
   public:
-    Charactor();
+    Charactor(const Window *_mainWindow);
     ~Charactor();
 
     void update();
