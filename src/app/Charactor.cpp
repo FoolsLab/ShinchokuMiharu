485601b3a92bd4e0658326f276c8886b0ea7099e
Kiterai <kiterai@foolslab.net>
2022-04-14 09:33:55 +0900

    依存関係の逆転

--- src/app/Charactor.cpp
+++ src/app/Charactor.cpp
@@ -2,25 +2,25 @@
 #include <GLFW/glfw3.h>
 #include <gl/GL.h>
 
-Charactor::Charactor(Window *const _mainWindow)
+Charactor::Charactor(IWindow& _mainWindow)
     : mainWindow(_mainWindow), tex1("./assets/test.png") {
     setWindowSize({512, 512});
 }
 
 Charactor::~Charactor() {}
 
-void Charactor::setWindowPos(Point pos) { mainWindow->setWindowPos(pos); }
+void Charactor::setWindowPos(Point pos) { mainWindow.setWindowPos(pos); }
 
 void Charactor::setWindowSize(Size size) {
-    mainWindow->setWindowSize(size);
+    mainWindow.setWindowSize(size);
     glViewport(0, 0, size.x, size.y);
 }
 
 void Charactor::update() {
-    auto context = mainWindow->renderBegin();
+    auto context = mainWindow.renderBegin();
 
     glClearColor(0, 0, 0, 0.5);
     glClear(GL_COLOR_BUFFER_BIT);
 
-    context.Draw(tex1, {150, 100});
+    context->Draw(tex1, {150, 100});
 }
